5462e5c60fd6c7c4520ca4abafb7e6c5bc98ed35
Norton Batista Abdala <49415914+nortonbatista@users.noreply.github.com>
2025-09-03 19:43:12 -0300

    Create succ and plus operations

--- index.ts
+++ index.ts
@@ -4,8 +4,14 @@ const ONE: IDENTITY<IDENTITY> = f => x => f(x)
 const TWO: IDENTITY<IDENTITY> = f => x => f(f(x))
 const THREE: IDENTITY<IDENTITY> = f => x => f(f(f(x)))
 
+const SUCC: IDENTITY<IDENTITY<IDENTITY>> = n => f => x => f(n(f)(x))
+const PLUS: IDENTITY<IDENTITY<IDENTITY<IDENTITY>>> = m => n => f => x => m(f)(n(f)(x))
+
 const toNumber = (n: IDENTITY<IDENTITY<number>>) => n(i => ++i)(0)
 
 console.log(toNumber(ONE))
 console.log(toNumber(TWO))
 console.log(toNumber(THREE))
+
+console.log(toNumber(SUCC(THREE)))
+console.log(toNumber(PLUS(SUCC(THREE))(ONE)))
