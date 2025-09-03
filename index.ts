3522a4848a735de34a59927fc1eaecec9bef794a
Norton Batista Abdala <49415914+nortonbatista@users.noreply.github.com>
2025-09-03 19:50:36 -0300

    Create MULTIPLY operation

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ const THREE: IDENTITY<IDENTITY> = f => x => f(f(f(x)))
 
 const SUCC: IDENTITY<IDENTITY<IDENTITY>> = n => f => x => f(n(f)(x))
 const PLUS: IDENTITY<IDENTITY<IDENTITY<IDENTITY>>> = m => n => f => x => m(f)(n(f)(x))
+const MULTIPLY: IDENTITY<IDENTITY<IDENTITY<IDENTITY>>> = m => n => f => x => m(n(f))(x)
 
 const toNumber = (n: IDENTITY<IDENTITY<number>>) => n(i => ++i)(0)
 
@@ -15,3 +16,7 @@ console.log(toNumber(THREE))
 
 console.log(toNumber(SUCC(THREE)))
 console.log(toNumber(PLUS(SUCC(THREE))(ONE)))
+
+console.log(toNumber(MULTIPLY(TWO)(THREE)))
+console.log(toNumber(MULTIPLY(THREE)(TWO)))
+console.log(toNumber(MULTIPLY(SUCC(THREE))(TWO)))
