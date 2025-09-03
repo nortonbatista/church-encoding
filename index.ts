1f998090acf8df72b886cfafecb37d220749145f
Norton Batista Abdala <49415914+nortonbatista@users.noreply.github.com>
2025-09-03 18:54:08 -0300

    Create church numerals

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
 type IDENTITY<T = any> = (x: T) => T
+
+const ONE: IDENTITY<IDENTITY> = f => x => f(x)
+const TWO: IDENTITY<IDENTITY> = f => x => f(f(x))
+const THREE: IDENTITY<IDENTITY> = f => x => f(f(f(x)))
+
+const toNumber = (n: IDENTITY<IDENTITY<number>>) => n(i => ++i)(0)
+
+console.log(toNumber(ONE))
+console.log(toNumber(TWO))
+console.log(toNumber(THREE))
